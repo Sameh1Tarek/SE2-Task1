2181fd8d7dd62372feeaeb03f8db24e344241135
Sameh1Tarek <Sameh1Tarek>
2023-04-19 18:19:21 +0200

    add Task V1

---
+++ SE2/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SE2.Models;
+
+namespace SE2.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student{Id=1 ,Name="Sameh" },
+                new Student{Id=2 ,Name="Ahmed" },
+                new Student{Id=3 ,Name="Alaa" },
+                new Student{Id=4 ,Name="Zeyad" }
+            };
+            return View(students);
+        }
+    }
+}
---
+++ SE2/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace SE2.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
